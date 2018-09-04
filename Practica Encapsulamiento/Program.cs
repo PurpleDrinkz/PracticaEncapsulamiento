2f98263e2a080a9569434597ba5bd398d3abf41a
Benjamín Ortega García <benjamin01234@hotmail.com>
2018-09-03 21:34:48 -0700

    Se aprendió a encapsular variables.

---
+++ Practica Encapsulamiento/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Practica_Encapsulamiento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Contacto contactoUno = new Contacto();
+
+            contactoUno.Nombre = "Benjamin Ortega";
+            contactoUno.Direccion = "Tamaulipas 532";
+            contactoUno.CorreoElectronico = "xbenja123@gmail.com";
+            contactoUno.TelefonoCasa = "4223258";
+            contactoUno.TelefonoCelular = "6421166822";
+
+            Console.WriteLine("Nombre: " + contactoUno.Nombre);
+            Console.WriteLine("Dirección: " + contactoUno.Direccion);
+            Console.WriteLine("Correo: " + contactoUno.CorreoElectronico);
+            Console.WriteLine("Telefono: " + contactoUno.TelefonoCasa);
+            Console.WriteLine("Celular: " + contactoUno.TelefonoCelular);
+
+            Console.ReadLine();
+        }
+
+    }
+}
